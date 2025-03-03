1ccd02f00e958b5badee43abff695bce995924e5
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-03-02 22:21:47 -0300

    ADD: Pinagem da placa NDA

---
+++ include/pinagem.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+// Pinagem placa NDA
+
+#define PINO_LED 0
+
+#define PINO_MOD_START 1
+
+#define PINO_IN1_ME 3
+#define PINO_IN2_ME 4
+#define PINO_IN1_MD 5
+#define PINO_IN2_MD 6
+
+#define PINO_SENSOR_LATERAL_DIR 7
+#define PINO_SENSOR_FRONTAL 10
+#define PINO_SENSOR_LATERAL_ESQ 21
+
+#define PINO_SENSOR_BORDA 20
