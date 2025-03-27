d102b286fbccabe80e07114bf0a5a2316a6a8ee1
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-03-26 23:01:06 -0300

    ADD: Arquivo DRV8870

---
+++ include/locomocao/DRV8870.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <Arduino.h>
+
+class DRV8870 {
+    private:
+        int _pino1;
+        int _pino2;
+
+        int _canalPino1;
+        int _canalPino2;
+
+        int _valorMaximoDePotencia;
+
+        int _valorDePotencia = 0;
+
+    public:
+
+        bool motorInvertido = false;
+        
+
+    DRV8870(int pino1, int pino2, int canalPino1, int canalPino2, int frequenciaPWM, int resolucaoPWM):
+        _pino1(pino1),
+        _pino2(pino2),
+        _canalPino1(canalPino1),
+        _canalPino2(canalPino2),
+
+        _valorMaximoDePotencia(pow(2, resolucaoPWM) - 1)
+    {
+        ledcSetup(_canalPino1, frequenciaPWM, resolucaoPWM);
+        ledcSetup(_canalPino2, frequenciaPWM, resolucaoPWM);
+
+        ledcAttachPin(_pino1, _canalPino1);
+        ledcAttachPin(_pino2, _canalPino2);
+    }
+
+    void parar() {
+        ledcWrite(_canalPino1, _valorMaximoDePotencia);
+        ledcWrite(_canalPino2, _valorMaximoDePotencia);
+
+        _valorDePotencia = 0;
+    }
+
+};
