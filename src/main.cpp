bb0f8e6047cffbf47f8eb8727d8f199c7514e04e
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-05-29 11:22:20 -0300

    ADD: Lógica de sensores

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,18 @@
 #include <DRV8870.hpp>
 
 DRV8870 motorEsquerdo(PINO_MOTOR_ESQUERDO_1,
-                     PINO_MOTOR_ESQUERDO_2,
-                     CANAL_PWM_MOTOR_ESQUERDO_1,
-                     CANAL_PWM_MOTOR_ESQUERDO_2,
-                     FREQUENCIA_PWM_MOTOR_ESQUERDO,
-                     RESOLUCAO_PWM_MOTOR_ESQUERDO);
+                      PINO_MOTOR_ESQUERDO_2,
+                      CANAL_PWM_MOTOR_ESQUERDO_1,
+                      CANAL_PWM_MOTOR_ESQUERDO_2,
+                      FREQUENCIA_PWM_MOTOR_ESQUERDO,
+                      RESOLUCAO_PWM_MOTOR_ESQUERDO);
 
 DRV8870 motorDireito(PINO_MOTOR_DIREITO_1,
-                      PINO_MOTOR_DIREITO_2,
-                      CANAL_PWM_MOTOR_DIREITO_1,
-                      CANAL_PWM_MOTOR_DIREITO_2,
-                      FREQUENCIA_PWM_MOTOR_DIREITO,
-                      RESOLUCAO_PWM_MOTOR_DIREITO);
+                     PINO_MOTOR_DIREITO_2,
+                     CANAL_PWM_MOTOR_DIREITO_1,
+                     CANAL_PWM_MOTOR_DIREITO_2,
+                     FREQUENCIA_PWM_MOTOR_DIREITO,
+                     RESOLUCAO_PWM_MOTOR_DIREITO);
 
 leituraModStart = digitalRead(PINO_MOD_START);
 
