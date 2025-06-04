afdb57639cef8b3256b1dada1ae2a03bc93b330b
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-06-04 04:16:06 -0300

    FIX: Ajustes na main

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,6 @@
 #include <Arduino.h>
-#include <DRV8870.hpp>
-
-DRV8870 motorEsquerdo(PINO_MOTOR_ESQUERDO_1,
-                      PINO_MOTOR_ESQUERDO_2,
-                      CANAL_PWM_MOTOR_ESQUERDO_1,
-                      CANAL_PWM_MOTOR_ESQUERDO_2,
-                      FREQUENCIA_PWM_MOTOR_ESQUERDO,
-                      RESOLUCAO_PWM_MOTOR_ESQUERDO);
-
-DRV8870 motorDireito(PINO_MOTOR_DIREITO_1,
-                     PINO_MOTOR_DIREITO_2,
-                     CANAL_PWM_MOTOR_DIREITO_1,
-                     CANAL_PWM_MOTOR_DIREITO_2,
-                     FREQUENCIA_PWM_MOTOR_DIREITO,
-                     RESOLUCAO_PWM_MOTOR_DIREITO);
-
-leituraModStart = digitalRead(PINO_MOD_START);
-
-enum Estados {
-    ESTRATEGIA_INICIAL_PRIMARIA,
-    ESTRATEGIA_INICIAL_SECUNDARIA,
-    ESTRATEGIA_INICIAL_TERCIARIA,
-    ESTRATEGIA_BUSCA,
-    ESTRATEGIA_PERSEGUICAO,
-    ESTRATEGIA_BUSCA_POS_PERSEGUICAO
-}
+#include "eletronica/configuracoes.hpp"
+#include "locomocao/configuracoes.hpp"
 
 void setup() {
     pinMode(PINO_LED, OUTPUT);
@@ -32,8 +8,7 @@ void setup() {
 }
 
 void loop() {
-
-    if leituraModStart == HIGH {
+    if (leituraModStart == HIGH) {
         digitalWrite(PINO_LED, HIGH);
         
         
@@ -41,9 +16,7 @@ void loop() {
 
     else {
         digitalWrite(PINO_LED, LOW);
-        parar.motorEsquerdo();
-        parar.motorDireito();
+        motorDireito.parar();
+        motorEsquerdo.parar();
     }
-
-
 }
