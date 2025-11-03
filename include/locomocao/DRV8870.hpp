759300de549b8d063a623e34118852020a555a72
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-11-03 03:29:28 -0300

    REFACTOR: Otimizando classe

--- include/locomocao/DRV8870.hpp
+++ include/locomocao/DRV8870.hpp
@@ -13,31 +13,32 @@ class DRV8870 {
         static constexpr int FREQUENCIA_PWM = 20000;
         static constexpr int RESOLUCAO_PWM = 12;
         static constexpr int POTENCIA_MAXIMA = (1 << RESOLUCAO_PWM) - 1;
+        static constexpr int POTENCIA_MINIMA = -POTENCIA_MAXIMA;
+        static constexpr uint8_t POTENCIA_NEUTRA = 0;
         int _potencia;
 
     public:
         DRV8870(uint8_t _pinoIN1, uint8_t _pinoIN2, uint8_t _canalIN1, uint8_t _canalIN2) 
                 : pinoIN1(_pinoIN1), pinoIN2(_pinoIN2), canalIN1(_canalIN1), canalIN2(_canalIN2) {
-        }
-
-        void initDriver() {
             ledcAttachPin(pinoIN1, canalIN1);
             ledcAttachPin(pinoIN2, canalIN2);
 
             ledcSetup(canalIN1, FREQUENCIA_PWM, RESOLUCAO_PWM);
             ledcSetup(canalIN2, FREQUENCIA_PWM, RESOLUCAO_PWM);
+
+            parar(false);
         }
 
         void setVelocidade(int potencia) {
-            potencia = constrain(potencia, -POTENCIA_MAXIMA, POTENCIA_MAXIMA);
+            potencia = constrain(potencia, POTENCIA_MINIMA, POTENCIA_MAXIMA);
 
-            if (potencia > 0) {
+            if (potencia > POTENCIA_NEUTRA) {
                 ledcWrite(canalIN1, potencia);
-                ledcWrite(canalIN2, 0);
+                ledcWrite(canalIN2, POTENCIA_NEUTRA);
             } 
             
-            else if (potencia < 0) {
-                ledcWrite(canalIN1, 0);
+            else if (potencia < POTENCIA_NEUTRA) {
+                ledcWrite(canalIN1, POTENCIA_NEUTRA);
                 ledcWrite(canalIN2, abs(potencia));
             } 
             
@@ -55,18 +56,18 @@ class DRV8870 {
             }
 
             else {
-                ledcWrite(canalIN1, 0);
-                ledcWrite(canalIN2, 0);
+                ledcWrite(canalIN1, POTENCIA_NEUTRA);
+                ledcWrite(canalIN2, POTENCIA_NEUTRA);
             }
 
-            _potencia = 0;
+            _potencia = POTENCIA_NEUTRA;
         }
 
         int getPotencia() const {
             return _potencia;
         }
 
-        static int getPotenciaMaxima() {
+        static constexpr int getPotenciaMaxima() {
             return POTENCIA_MAXIMA;
         }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,12 @@
 void setup() {
     initBLE();
 
-    motorDireito.initDriver();
-    motorEsquerdo.initDriver();
-
     pinMode(PINO_LED, OUTPUT);
     pinMode(PINO_MOD_START, INPUT);
 }
 
 void loop() {
-    BLE.pool();
-
-    switch (leituraModStart) {
+    switch (lerModStart()) {
         case HIGH:
             digitalWrite(PINO_LED, HIGH);
 
